2910e12ccbcd723a527a4252de647c1ee16cf0dd
Vivek <365anu65@gmail.com>
2023-12-19 13:45:48 +0530

    Proper in mobile view of you know me..

--- app/Facts.tsx
+++ app/Facts.tsx
@@ -1,7 +1,7 @@
 export default function Facts(props: { emoji: any; fact: any; des: any }) {
   return (
     <>
-      <div className="w-32 lg:w-48 m-4 p-4 rounded-xl font-mono font-extrabold flex flex-col justify-between">
+      <div className="w-32 lg:w-48 m-2 p-2 lg:m-4 lg:p-4 rounded-xl font-mono font-extrabold flex flex-col justify-between">
         <div className="flex flex-col my-2">
           <div className="text-4xl">{props.emoji}</div>
           <div className="my-4">{props.fact}</div>
