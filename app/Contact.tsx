34efa545bb1fb0113075340791f8b76ff66ee9aa
Vivek <365anu65@gmail.com>
2023-12-24 22:34:03 +0530

    Contact form design is done..

--- app/Contact.tsx
+++ app/Contact.tsx
@@ -30,12 +30,27 @@ const Contact: React.FC = () => {
         Lets Collaborate
       </div>
       <div className="flex my-12">
-        <div className="w-1/2"></div>
-        <div className="w-1/2 p-6">
+        <div className="hidden lg:inline w-1/2 bg-[url('https://www.coworker.com/mag/wp-content/uploads/2017/08/teamwork-discussion-meeting-brainstorming-start-P59ACVM-1280x640.jpg')] bg-cover m-6"></div>
+        <div className="w-full lg:w-1/2 p-6">
           <div className="w-full">
             <form onSubmit={handleSubmit} className="w-full">
-              <div className="mb-4 flex items-center justify-start space-x-4">
-                <div className="text-2xl">{"~"}</div>
+              <div className="mb-12 flex items-center justify-start">
+                <div className="bg-white p-1 text-2xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-black"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+                </div>
                 <input
                   type="text"
                   id="fullName"
@@ -43,46 +58,84 @@ const Contact: React.FC = () => {
                   name="fullName"
                   value={formData.fullName}
                   onChange={handleChange}
-                  className="w-full bg-transparent border-b border-slate-300 py-1 focus:outline-none"
+                  className="w-full bg-transparent border-b border-slate-300 py-1 px-2 text-white font-bold focus:outline-none"
                   required
                 />
               </div>
-
-              <div className="mb-4">
-                <label htmlFor="email" className="block font-semibold mb-2">
-                  Email:
-                </label>
+              <div className="mb-12 flex items-center justify-start">
+                <div className="bg-white p-1 text-2xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-black"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+                    />
+                  </svg>
+                </div>
                 <input
                   type="email"
                   id="email"
+                  placeholder="Your Email Address"
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
-                  className="w-full p-2 border rounded"
+                  className="w-full bg-transparent border-b border-slate-300 py-1 px-2 text-white font-bold focus:outline-none"
                   required
                 />
               </div>
-
-              <div className="mb-4">
-                <label htmlFor="message" className="block font-semibold mb-2">
-                  Message:
-                </label>
+              <div className="mb-12 flex items-start justify-start">
+                <div className="bg-white p-1 text-2xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-black"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+                </div>
                 <textarea
                   id="message"
+                  placeholder="Your Message"
                   name="message"
-                  rows={4}
                   value={formData.message}
+                  rows={5}
                   onChange={handleChange}
-                  className="w-full p-2 border rounded"
+                  className="w-full bg-transparent border-b border-l border-slate-300 py-1 px-2 text-white font-bold focus:outline-none resize-none"
                   required
                 />
               </div>
-
-              <button
-                type="submit"
-                className="bg-green-500 text-white py-2 px-4 rounded"
-              >
-                Submit
+              <button type="submit" className="flex">
+                <div className="bg-white text-black p-1 text-2xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                    />
+                  </svg>
+                </div>
+                <div className="py-1 px-2 text-gray-400 font-bold">Send</div>
               </button>
             </form>
           </div>
